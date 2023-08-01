7c8fa386f310d32a36cf4b3f1c1821381ac7ddfd
JackDaw1 <iaroshenko.ga@gmail.com>
2023-08-01 14:40:23 +0700

    first commit

---
+++ Surf_test_CV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Surf_test_CV
+//
+//  Created by Galina Iaroshenko on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
